53545a2686c1581beb68b2d770e85684a24c74c7
Stav Green <stavgn@gmail.com>
2021-08-18 23:39:42 +0300

    config package

---
+++ packages/autoTestConfig/src/index.js
@@ -0,0 +1,21 @@
+import config from './config.js'
+const ENV = process.env.NODE_ENV;
+
+const megred = Object.assign({}, config.default, config[ENV] || {});
+
+const withEnvOverrides = Object.keys(megred).reduce((acc, next) => {
+    acc[next] = process.env[next] || megred[next];
+    return acc;
+}, {})
+
+const get = (key) => {
+    return withEnvOverrides[key];
+}
+
+const env = () => ENV
+
+export default {
+    get,
+    env,
+    isOn
+}
