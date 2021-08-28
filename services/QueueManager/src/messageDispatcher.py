8a592529cb747112226f45e0072d0ffe3bb6b2de
Stav Green <stavgn@gmail.com>
2021-08-28 21:29:53 +0300

    message dispatcher done

--- services/QueueManager/src/messageDispatcher.py
+++ services/QueueManager/src/messageDispatcher.py
@@ -1,7 +1,22 @@
+import pika
+import json
+import uuid
+
 
 class MessageDispatcher():
-    def __init__(self):
-        pass
+    def __init__(self, queueUrl, queueName):
+        self.queueName = queueName
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(queueUrl))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=queueName)
 
-    def dispatch(self):
-        return 123
+    def dispatch(self, message):
+        message['id'] = str(uuid.uuid4())
+        print(message)
+        self.channel.basic_publish(
+            exchange='',
+            routing_key=self.queueName,
+            body=json.dumps(message)
+        )
+        return message['id']
--- services/QueueManager/src/server.py
+++ services/QueueManager/src/server.py
@@ -1,14 +1,28 @@
+from os import environ
 from flask import Flask, make_response, jsonify, request
 from messageDispatcher import MessageDispatcher
 
-dispatcher = MessageDispatcher()
+
+queueUrl = environ.get('QUEUE_URL') or 'localhost'
+queueName = environ.get('QUEUE_NAME') or 'default'
+
+dispatcher = MessageDispatcher(queueUrl, queueName)
 app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return make_response(jsonify(
+        {'QUEUE_URL': queueUrl,
+         'QUEUE_NAME': queueName
+         }
+    ))
 
 
 @app.route('/emit', methods=['POST'])
 def emit():
     message = request.get_json()
-    id = dispatcher.dispatch()
+    id = dispatcher.dispatch(message)
     return make_response(jsonify({'id': id}), 201)
 
 
